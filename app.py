9e506a004b3a27b13fd94b756d11ffbe33dc9962
Garrett Schaeffer <gschaeffer@gmail.com>
2018-05-08 22:21:23 -0500

    Add AWS event sample data

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from dictutil import find_key
+import json
 
 example = {'app_url': '', 'models': [{'perms': {'add': True, 'change': True, 'delete': True, 'etc': [{'one': True}]}, 'add_url': '/admin/cms/news/add/', 'admin_url': '/admin/cms/news/', 'name': ''}], 'has_module_perms': True, 'name': u'CMS', 'admin_url': '/my_account/admin/'}
 
-for x in find_key('models', example):
+with open("event.json", "r") as f:
+  data = f.read()
+example = json.loads(data)
+
+for x in find_key('accountId', example):
     print(x)
 
-[print(x) for x in find_key('models', example)]
+[print(x) for x in find_key('accountId', example)]
 
-v = next(find_key('modelss', example), None)
+v = next(find_key('accountId', example), None)
 print(v)
